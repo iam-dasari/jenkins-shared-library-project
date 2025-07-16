962a5b35a09444e8952e94c6c85bb79de5406e4e
Bullaiah <bullaiahd@gmail.com>
2025-07-16 17:59:46 +0530

    Libraries

--- vars/javaEKS.groovy
+++ vars/javaEKS.groovy
@@ -1,8 +1,8 @@
-// def gitCheckout(Map configMap) {
-//     def BRANCH = configMap.get('branch')
-//     def URL = configMap.get('url')
-//     git branch: ${BRANCH}, url: ${URL}
-// }
+def gitCheckout(Map configMap) {
+    def BRANCH = configMap.get('branch')
+    def URL = configMap.get('url')
+    git branch: ${BRANCH}, url: ${URL}
+}
 
 def call() {
     echo "Call Jenkins shared library"
