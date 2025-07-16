a0fa45e5108adc08b9d72c1b1f59896ec368db05
Bullaiah <bullaiahd@gmail.com>
2025-07-16 17:47:44 +0530

    Libraries

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,5 +1,9 @@
-def gitCheckout(Map configMap) {
-    def BRANCH = configMap.get('branch')
-    def URL = configMap.get('url')
-    git branch: ${BRANCH}, url: ${URL}
+// def gitCheckout(Map configMap) {
+//     def BRANCH = configMap.get('branch')
+//     def URL = configMap.get('url')
+//     git branch: ${BRANCH}, url: ${URL}
+// }
+
+def call() {
+    echo "Call Jenkins shared library"
 }
