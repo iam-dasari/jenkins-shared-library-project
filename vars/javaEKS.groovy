598e3e5567c34a0a8bdfa603ceeef2860aa7412e
DasariBullaiah <dasaridevops2025@gmail.com>
2025-12-28 19:39:36 +0530

    shared library

--- vars/javaEKS.groovy
+++ vars/javaEKS.groovy
@@ -1,9 +1,3 @@
-def gitCheckout(Map configMap) {
-    def BRANCH = configMap.get('branch')
-    def URL = configMap.get('url')
-    git branch: ${BRANCH}, url: ${URL}
-}
-
 def call() {
     echo "Call Jenkins shared library"
 }
